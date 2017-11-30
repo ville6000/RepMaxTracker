c8bc79a8f1d188430eb42e46c20ad4b731af4d01
Ville Viklund <ville.viklund@morgan.fi>
2017-11-30 23:40:07 +0200

    Add line charts
    Adds line chart per exercise displaying estimeated one rep maxes

---
+++ src/components/ChartsView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="charts-view container">
+    <div class="row">
+      <div class="col col-lg-6" v-for="exercise in exercises" :key="exercise.id">
+        <rep-max-line-chart :sets="exerciseSets(exercise.id)" :exerciseName="exercise.name"></rep-max-line-chart>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import RepMaxLineChart from '@/components/RepMaxLineChart'
+
+  export default {
+    name: 'ChartsView',
+    components: {
+      RepMaxLineChart
+    },
+    methods: {
+      exerciseSets (id) {
+        return this.$store.getters.setsByExerciseId(id)
+      }
+    },
+    computed: {
+      exercises () {
+        return this.$store.getters.allExercises
+      }
+    }
+  }
+</script>
---
+++ src/components/RepMaxLineChart.vue
@@ -0,0 +1,26 @@
+<script>
+  import { Line } from 'vue-chartjs'
+  import oneRepMax from '@/mixins/oneRepMax'
+
+  export default {
+    name: 'RepMaxDotChart',
+    extends: Line,
+    mixins: [oneRepMax],
+    props: ['sets', 'exerciseName'],
+    mounted () {
+      if (this.sets.length === 0) {
+        return
+      }
+
+      this.renderChart({
+        labels: this.sets.map(s => s.date),
+        datasets: [
+          {
+            label: `Estimated 1 rep maxes for ${this.exerciseName}`,
+            data: this.sets.map(s => this.oneRepMax(s))
+          }
+        ]
+      })
+    }
+  }
+</script>
